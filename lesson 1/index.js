4b73d1e22cbcf3ccaab188ff5a53e0029c19dc43
Alex <tulen.spb@mail.ru>
2020-05-30 11:52:56 +0300

    lesson 1

---
+++ lesson 1/index.js
@@ -0,0 +1,18 @@
+//выводим звёздачками число 10
+console.log("   *   **** ");
+console.log("  **   *  * ");
+console.log(" * *   *  * ");
+console.log("   *   *  * ");
+console.log("   *   *  * ");
+console.log(" ****  **** ");
+
+console.log("");
+
+//выводим решёточками букву В
+console.log("#####");
+console.log("#    #");
+console.log("#    #");
+console.log("#####");
+console.log("#    #");
+console.log("#    #");
+console.log("#####");
