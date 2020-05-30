47f560d47e6974c0beca6270f3e88b8ad268757e
Alex <tulen.spb@mail.ru>
2020-05-30 12:31:33 +0300

    lesson 2

---
+++ lesson 2/index.js
@@ -0,0 +1,11 @@
+const num1 = 6;
+const num2 = 4;
+
+const sum = num1 + num2;
+console.log(num1, "+", num2, "=", sum);
+
+console.log(num1, "-", num2, "=", num1 - num2 );
+
+console.log(`${num1} * ${num2} = ${num1 * num2}`);
+
+console.log(`${num1} / ${num2} = ${num1 / num2}`);
