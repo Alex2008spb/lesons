f1eb9d2124e16e8cbff0b2ce5afa9edd9756b662
alex <tulen.spb@mail.ru>
2021-01-30 22:24:37 +0300

    lesson 11(factorial,Bigint)

--- lesson 11/index.js
+++ lesson 11/index.js
@@ -1,2 +1,7 @@
 const factorial = require("../modules/factorial");
-console.log(factorial(3))
+const num = factorial(24, 15 );
+console.log(num);
+let zeros = 0;
+for (let div = BigInt(10) ; num % div === 0n; zeros++ , div *= BigInt(10));
+
+console.log(zeros);
--- modules/factorial.js
+++ modules/factorial.js
@@ -1,20 +1,22 @@
 /**
  * @summary Функция возвращает факториал числа.
  * @param {number} num Число факториал которого нужно вычислить.
+ * @param {number} start Необязательный параметр: число с которого начинается вычисление факториала. По-умолчанию 1.
  * @returns {number}
  * @example factorial (5) -> 120
+ * @example factorial (5,3) -> 60
  */
-// module.exports = function (num) {
-//     let rtn = 1;
-//
-//     for ( let i = 1; i <= num; i++ )
-//         rtn *= i;
-//
-//     return rtn;
-// }
+function factorial(num, start = 1) {
+    let rtn = BigInt(1);
 
-function factorial (num){
-    if (num === 0) return 1;
-    return num * factorial(num - 1);
+    for ( let i = start; i <= num; i++ )
+        rtn *= BigInt(i);
+
+    return rtn;
 }
+
+// function factorial (num){
+//     if (num === 0) return 1;
+//     return num * factorial(num - 1);
+// }
 module.exports = factorial;
